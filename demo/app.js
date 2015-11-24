4043929c3173c19b5493217e3f467b2b532a2100
Greg <greg@flashtalking.com>
2015-11-24 17:19:29 +0000

    v1.3.0 in dev - demo updated

--- demo/app.js
+++ demo/app.js
@@ -1,4 +1,4 @@
-riot.tag2('demo-app', '<button onclick="{goWelcome}">Welcome</button> <button onclick="{goAbout}">About</button> <button onclick="{startRouter}">Start</button> <button onclick="{stopRouter}">Stop</button> <button onclick="{goTemplateUrl}">Template Url</button> <button onclick="{goTemplateUrlWithId}">Template Url with Id</button> <button onclick="{usePushState}">Use pushState</button> <button onclick="{useLocationHash}">Use location hash</button>', '', '', function(opts) {
+riot.tag2('demo-app', '<button onclick="{startRouter}">Start</button> <button onclick="{stopRouter}">Stop</button> <button onclick="{usePushState}">Use PushState</button> <button onclick="{useLocationHash}">Use Location Hash</button> <hr> <pre><code>{JSON.stringify(router.current, null, 2)}</code></pre> <hr> <button onclick="{goHome}">Go Home</button> <button onclick="{goUnknown}">Unknown State</button> <button onclick="{goWelcome}">Welcome</button> <button onclick="{goAbout}">About</button> <button onclick="{goChild}">Child State</button> <button onclick="{goResolve}">Resolve</button> <button onclick="{goTemplateUrl}">Template Url</button> <button onclick="{goTemplateUrlWithId}">Template Url with Id</button>', '', '', function(opts) {
 var _this = this;
 
 this.mixin('rg.router');
@@ -7,27 +7,68 @@ this.router.add({
 	name: 'home',
 	url: ''
 }).add({
-	name: 'about.template',
-	url: '/about/:collection/:id/:action'
+	name: 'template',
+	url: 'template/:collection/:id/:action'
+}).add({
+	name: 'template.child',
+	title: 'Template Child'
 }).add({
 	name: 'page',
 	url: 'page/:page',
-	opts: {
+	data: {
 		a: 1
 	}
+}).add({
+	name: 'page.child',
+	subtitle: 'child state',
+	moredata: {
+		b: 2
+	}
+}).add({
+	name: 'resolve',
+	url: 'resolve/promise',
+	message: 'Promise example',
+	resolve: function resolve() {
+		return new Promise(function (resolve) {
+			window.setTimeout(resolve, 2000);
+		});
+	}
 });
+
+this.goHome = function (stateName) {
+	return _this.router.go('home');
+};
+this.goUnknown = function (stateName) {
+	return _this.router.go('unknown');
+};
 
 this.goWelcome = function () {
 	return _this.router.go('page', { page: 'welcome', title: 'Welcome' });
 };
+
 this.goAbout = function () {
 	return _this.router.go('page', { page: 'about', title: 'About Us' });
 };
+this.goChild = function () {
+	return _this.router.go('page.child');
+};
+this.goResolve = function () {
+	return _this.router.go('resolve');
+};
+
 this.goTemplateUrl = function () {
-	return _this.router.go('about.template', { collection: 'biscuits' });
+	return _this.router.go('template', { collection: 'biscuits' });
 };
 this.goTemplateUrlWithId = function () {
-	return _this.router.go('about.template', { collection: 'biscuits', id: '470129', action: 'edit', whatever: 'youwant' });
+	return _this.router.go('template', {
+		collection: 'biscuits',
+		id: '470129',
+		action: 'edit',
+		whatever: 'youwant'
+	});
+};
+this.goTemplateChild = function () {
+	return _this.router.go('template.child');
 };
 
 this.startRouter = function () {
--- demo/app.tag
+++ demo/app.tag
@@ -1,37 +1,76 @@
 <demo-app>
 
-	<button onclick="{ goWelcome }">Welcome</button>
-	<button onclick="{ goAbout }">About</button>
 	<button onclick="{ startRouter }">Start</button>
 	<button onclick="{ stopRouter }">Stop</button>
+	<button onclick="{ usePushState }">Use PushState</button>
+	<button onclick="{ useLocationHash }">Use Location Hash</button>
+	<hr>
+	<pre><code>{JSON.stringify(router.current, null, 2)}</code></pre>
+	<hr>
+	<button onclick="{ goHome }">Go Home</button>
+	<button onclick="{ goUnknown }">Unknown State</button>
+	<button onclick="{ goWelcome }">Welcome</button>
+	<button onclick="{ goAbout }">About</button>
+	<button onclick="{ goChild }">Child State</button>
+	<button onclick="{ goResolve }">Resolve</button>
 	<button onclick="{ goTemplateUrl }">Template Url</button>
 	<button onclick="{ goTemplateUrlWithId }">Template Url with Id</button>
-	<button onclick="{ usePushState }">Use pushState</button>
-	<button onclick="{ useLocationHash }">Use location hash</button>
 
 	<script>
 		this.mixin('rg.router')
 
 		this.router.add({
-			name: 'home',
-			url: ''
-		})
-		.add({
-			name: 'about.template',
-			url: '/about/:collection/:id/:action'
-		})
-		.add({
-			name: 'page',
-			url: 'page/:page',
-			opts: {
-				a: 1
-			}
-		})
+				name: 'home',
+				url: ''
+			})
+			.add({
+				name: 'template',
+				url: 'template/:collection/:id/:action'
+			})
+			.add({
+				name: 'template.child',
+				title: 'Template Child'
+			})
+			.add({
+				name: 'page',
+				url: 'page/:page',
+				data: {
+					a: 1
+				}
+			})
+			.add({
+				name: 'page.child',
+				subtitle: 'child state',
+				moredata: {
+					b: 2
+				}
+			})
+			.add({
+				name: 'resolve',
+				url: 'resolve/promise',
+				message: 'Promise example',
+				resolve: () => new Promise(function (resolve) {
+					window.setTimeout(resolve, 2000)
+				})
+			})
+
+		this.goHome = stateName => this.router.go('home')
+		this.goUnknown = stateName => this.router.go('unknown')
 
 		this.goWelcome = () => this.router.go('page', { page: 'welcome', title: 'Welcome' })
+
 		this.goAbout = () => this.router.go('page', { page: 'about', title: 'About Us' })
-		this.goTemplateUrl = () => this.router.go('about.template', { collection: 'biscuits' })
-		this.goTemplateUrlWithId = () => this.router.go('about.template', { collection: 'biscuits', id: '470129', action: 'edit', whatever: 'youwant' })
+		this.goChild = () => this.router.go('page.child')
+		this.goResolve = () => this.router.go('resolve')
+
+		this.goTemplateUrl = () => this.router.go('template', { collection: 'biscuits' })
+		this.goTemplateUrlWithId = () => this.router.go('template', {
+			collection: 'biscuits',
+			id: '470129',
+			action: 'edit',
+			whatever: 'youwant'
+		})
+		this.goTemplateChild = () => this.router.go('template.child')
 
 		this.startRouter = () => this.router.start()
 		this.stopRouter = () => this.router.stop()
